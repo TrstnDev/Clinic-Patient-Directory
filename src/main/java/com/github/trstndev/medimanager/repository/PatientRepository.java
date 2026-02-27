eaf52262acd9f169d11ef1f44e25b39d78c0c2b9
TKriel_ST10467193 <krieltristan68@gmail.com>
2026-02-27 10:29:02 +0200

    Updated Patient Repository to better integrate with UUID security; added exact match search via RSA ID.

--- src/main/java/com/github/trstndev/medimanager/repository/PatientRepository.java
+++ src/main/java/com/github/trstndev/medimanager/repository/PatientRepository.java
@@ -3,20 +3,29 @@ package com.github.trstndev.medimanager.repository;
 import com.github.trstndev.medimanager.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, String> {
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
 
-    // 1. Search by Patient Name
+    // 1. Search by Patient First Name
     List<Patient> findByPatientNameContainingIgnoreCase(String patientName);
 
-    // 2. Search by ID
-    List<Patient> findByPatientIdContainingIgnoreCase(String patientId);
+    // 2. Search by Patient Surname
+    List<Patient> findByPatientSurnameContainingIgnoreCase(String patientSurname);
 
-    // 3. Search by Diagnosis
+    // 3. Search by Medical Record Number
+    List<Patient> findByPatientFileNumberContainingIgnoreCase(String patientFileNumber);
+
+    // 4. Exact match by national RSA ID number
+    Optional<Patient> findByPatientRsaId(String patientRsaId);
+
+    // 5. Search by Primary Diagnosis
     List<Patient> findByPrimaryDiagnosis_DiagnosisNameContainingIgnoreCase(String diagnosisName);
 
-    // 4. Search by Treating Physician's Name
+    // 6. Search by Treating Physician's Name
     List<Patient> findByTreatingPhysician_PhysicianNameContainingIgnoreCase(String physicianName);
 }
